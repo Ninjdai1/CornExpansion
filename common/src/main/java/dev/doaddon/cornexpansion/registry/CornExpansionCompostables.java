7505cc1de66d3ea05a8cad0e58a9fa14bfdfd1ec
Ninjdai <ninjdai@duck.com>
2024-06-28 15:37:15 +0200

    feat: Compostables registry

---
+++ common/src/main/java/dev/doaddon/cornexpansion/registry/CornExpansionCompostables.java
@@ -0,0 +1,23 @@
+package dev.doaddon.cornexpansion.registry;
+
+import net.minecraft.world.level.block.ComposterBlock;
+
+public class CornExpansionCompostables {
+    public static void init() {
+        ComposterBlock.COMPOSTABLES.put(CornExpansionObjects.POPCORN.get(), .2f);
+        ComposterBlock.COMPOSTABLES.put(CornExpansionObjects.SWEET_POPCORN.get(), .2f);
+        ComposterBlock.COMPOSTABLES.put(CornExpansionObjects.BUTTERY_POPCORN.get(), .2f);
+        ComposterBlock.COMPOSTABLES.put(CornExpansionObjects.CHEESY_POPCORN.get(), .2f);
+        ComposterBlock.COMPOSTABLES.put(CornExpansionObjects.CANDIED_POPCORN.get(), .2f);
+
+        ComposterBlock.COMPOSTABLES.put(CornExpansionObjects.CORN_ON_THE_COB.get(), .3f);
+        ComposterBlock.COMPOSTABLES.put(CornExpansionObjects.GRILLED_CORN.get(), .3f);
+
+        ComposterBlock.COMPOSTABLES.put(CornExpansionObjects.CORN_DOUGH.get(), .3f);
+        ComposterBlock.COMPOSTABLES.put(CornExpansionObjects.CORN_FLOUR.get(), .3f);
+        ComposterBlock.COMPOSTABLES.put(CornExpansionObjects.DRIED_CORN.get(), .2f);
+        ComposterBlock.COMPOSTABLES.put(CornExpansionObjects.CORNMEAL.get(), .3f);
+
+        ComposterBlock.COMPOSTABLES.put(CornExpansionObjects.GRANDMAS_CORNBREAD.get(), .6f);
+    }
+}
