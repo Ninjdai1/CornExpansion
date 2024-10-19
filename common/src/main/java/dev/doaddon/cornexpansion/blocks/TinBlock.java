133ef8a66e38fd1b0a44bd9316bd0c68c3b3db9a
Ninjdai <ninjdai@duck.com>
2024-10-19 13:27:51 +0200

    feat: colored tin textures

--- common/src/main/java/dev/doaddon/cornexpansion/blocks/TinBlock.java
+++ common/src/main/java/dev/doaddon/cornexpansion/blocks/TinBlock.java
@@ -1,10 +1,17 @@
 package dev.doaddon.cornexpansion.blocks;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraft.world.phys.shapes.BooleanOp;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
@@ -12,8 +19,10 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 
 public class TinBlock extends EffectFoodBlock{
+    public static final EnumProperty<DyeColor> COLOR;
     public TinBlock(Properties settings, int maxBites, FoodProperties foodComponent) {
         super(settings, maxBites, foodComponent);
+        this.registerDefaultState(this.defaultBlockState().setValue(COLOR, DyeColor.WHITE));
     }
 
     VoxelShape voxelShape = Shapes.join(Block.box(5, 0, 5, 11, 9, 11), Block.box(4, 7, 4, 12, 9, 12), BooleanOp.OR);
@@ -21,5 +30,15 @@ public class TinBlock extends EffectFoodBlock{
     @Override
     public @NotNull VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
         return voxelShape;
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        super.createBlockStateDefinition(builder);
+        builder.add(COLOR);
+    }
+
+    static {
+        COLOR = EnumProperty.create("color", DyeColor.class);
     }
 }
